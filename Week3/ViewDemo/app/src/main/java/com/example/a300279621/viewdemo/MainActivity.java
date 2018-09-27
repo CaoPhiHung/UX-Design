a6f0ce8df8fc3b74b453ce8c8f2b3cac499c9235
CaoPhiHung <caophihung8392@gmail.com>
2018-09-27 12:07:26 -0700

    week 3

--- Week3/ViewDemo/app/src/main/java/com/example/a300279621/viewdemo/MainActivity.java
+++ Week3/ViewDemo/app/src/main/java/com/example/a300279621/viewdemo/MainActivity.java
@@ -2,14 +2,20 @@ package com.example.a300279621.viewdemo;
 
 import android.graphics.Color;
 import android.graphics.SumPathEffect;
+import android.graphics.drawable.Drawable;
+import android.provider.SyncStateContract;
 import android.support.annotation.Dimension;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.Constraints;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 final TextView text = (TextView) findViewById(R.id.txtViewSample);
-                text.setText("Hello Henry \nHow are you today \n this is a tr\nasdasdasdasd");
+                text.setText("Hello Henry \nHow are you today");
                 text.setPadding(16,16,16,16);
                 text.setCompoundDrawablePadding(8);
                 text.setBackgroundColor(Color.BLACK);
@@ -35,24 +41,50 @@ public class MainActivity extends AppCompatActivity {
                 text.setOnLongClickListener(new View.OnLongClickListener() {
                     @Override
                     public boolean onLongClick(View v) {
-                        if(text.getText().toString().equals("")){
-                            text.setText("");
+                        if(text.getText().toString().equals("Hello Henry \nHow are you today")){
+                            text.setText("Changed Long Click Text");
                         }else{
-                            text.setText("");
+                            text.setText("New Line");
                         }
                         return true;
                     }
                 });
+
+//                ConstraintLayout.LayoutParams lp = new ConstraintLayout.LayoutParams(400, 600);
+//                text.setLayoutParams(lp);
+                text.getLayoutParams().height = 400;
+                text.getLayoutParams().width = 600;
+
             }
         });
 
-        final Button changeImage = (Button) findViewById(R.id.btnImageChange);
-        changeImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                ImageView img = (ImageView) findViewById(R.id.imageView);
-            }
-        });
+               final ImageView img = (ImageView) findViewById(R.id.imageView);
+                img.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (img.getDrawable().getConstantState().equals(
+                                getResources().getDrawable(R.drawable.puppy).getConstantState())) {
+                            img.setImageResource(R.drawable.darkbg2);
+                            Toast.makeText(MainActivity.this, "Changed", Toast.LENGTH_LONG).show();
+                        }else if(img.getDrawable().getConstantState().equals(
+                                getResources().getDrawable(R.drawable.darkbg2).getConstantState())){
+                            img.setImageResource(R.drawable.puppy);
+                        }
+                    }
+                });
+
+                final Button changeImage = (Button) findViewById(R.id.btnImageChange);
+                changeImage.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(img.getScaleType() == ImageView.ScaleType.FIT_XY){
+                            img.setScaleType(ImageView.ScaleType.FIT_CENTER);
+
+                        }else{
+                            img.setScaleType(ImageView.ScaleType.FIT_XY);
+                        }
+                    }
+                });
 
     }
 }
